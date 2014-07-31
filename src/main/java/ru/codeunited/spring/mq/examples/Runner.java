8cdc35476916c28691eee55be22cd9e995228f01
ikonovalov <igor_konovalov@mail.ru>
2014-07-31 15:25:21 +0400

    Create separated sender class and improve configuration

--- src/main/java/ru/codeunited/spring/mq/examples/Runner.java
+++ src/main/java/ru/codeunited/spring/mq/examples/Runner.java
@@ -1,14 +1,8 @@
 package ru.codeunited.spring.mq.examples;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessageCreator;
 
 import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.Session;
-import javax.jms.TextMessage;
-import java.util.Date;
 
 /**
  * Created by Igor on 2014.07.31.
@@ -20,25 +14,18 @@ public class Runner {
         context.registerShutdownHook();
 
         new Thread(new Runnable() {
+
+            private MQMessageSender sender;
+
+            {
+                sender = context.getBean(MQMessageSender.class);
+            }
+
             @Override
             public void run() {
-                final JmsTemplate template = context.getBean(JmsTemplate.class);
-                int counter = 0;
-                while (counter++ < 100) {
-                    template.send("ETP.Q1", new MessageCreator() {
-                        @Override
-                        public Message createMessage(Session session) throws JMSException {
-                            TextMessage tm = session.createTextMessage();
-                            tm.setText("Current time is " + new Date());
-                            tm.setJMSReplyTo(template.getDestinationResolver().resolveDestinationName(session, "ETP.DEFAULT.RESP", false));
-                            return tm;
-                        }
-                    });
-                    try {
-                        Thread.sleep(10);
-                    } catch (InterruptedException e) {
-                        break;
-                    }
+                int count = 100;
+                while(count-->0) {
+                    sender.send("Message with " + System.nanoTime());
                 }
             }
         }).start();
