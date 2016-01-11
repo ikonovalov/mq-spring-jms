640837cc5045265706ce52542856553bf9e9e110
ikonovalov <konovalov84@gmail.com>
2016-01-11 18:40:54 +0300

    Remove unused imports

--- src/main/java/ru/codeunited/jms/spring/JMSContainerConfiguration.java
+++ src/main/java/ru/codeunited/jms/spring/JMSContainerConfiguration.java
@@ -1,7 +1,6 @@
 package ru.codeunited.jms.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
